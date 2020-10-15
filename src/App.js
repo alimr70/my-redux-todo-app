323e519d1bb126d4230e1d0c5c5f73cf6a90bf4b
Ali Maher <ali.maher.mohammed@gmail.com>
2020-10-15 05:24:41 +0200

    Finished toolbar

--- src/App.js
+++ src/App.js
@@ -91,6 +91,34 @@ function App() {
                       <i className="icon icon-arrow"></i>
                     </div>
                   </div>
+                  <ul className="group-lists">
+                    <div>
+                      <li className="toolbar-item">
+                        <div className="toolbar-inner">
+                          <div className="toolbar-icon">
+                            <i className="icon icon-ham"></i>
+                          </div>
+                          <div className="toolbar-title">
+                            <span>My todo app</span>
+                          </div>
+                          <div className="toolbar-count">1</div>
+                        </div>
+                      </li>
+                    </div>
+                    <div>
+                      <li className="toolbar-item">
+                        <div className="toolbar-inner">
+                          <div className="toolbar-icon">
+                            <i className="icon icon-ham"></i>
+                          </div>
+                          <div className="toolbar-title">
+                            <span>El Nada app</span>
+                          </div>
+                          <div className="toolbar-count">1</div>
+                        </div>
+                      </li>
+                    </div>
+                  </ul>
                 </li>
               </div>
             </div>

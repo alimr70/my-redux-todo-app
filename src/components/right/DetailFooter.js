adb006a0a2674ef2f2ca3171a280eccc65b7fc32
Ali Maher <ali.maher.mohammed@gmail.com>
2020-10-29 09:58:49 +0200

    fixed detailbar footer again

--- src/components/right/DetailFooter.js
+++ src/components/right/DetailFooter.js
@@ -1,10 +1,19 @@
 import React from "react";
+import * as actions from "../../redux/actions/actions";
+import { useDispatch } from "react-redux";
 
 const DetailFooter = ({ currentTaskDate }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="toolbar-item detail-footer">
       <div className="toolbar-inner">
-        <div className="toolbar-icon">
+        <div
+          className="toolbar-icon"
+          onClick={() => {
+            dispatch(actions.openDetailbar(false));
+            dispatch(actions.setCurrentTask(null));
+          }}>
           <i className="icon icon-arrow"></i>
         </div>
         <div className="toolbar-title">

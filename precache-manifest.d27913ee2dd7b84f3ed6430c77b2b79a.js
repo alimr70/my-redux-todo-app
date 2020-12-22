62f98c9b28945f628047c53cd6969b7305e5fcea
Ali Maher <ali.maher.mohammed@gmail.com>
2020-12-22 16:34:15 +0200

    Updates

---
+++ precache-manifest.d27913ee2dd7b84f3ed6430c77b2b79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55317465f22f67cf9c09642170022961",
+    "url": "/my-redux-todo-app/index.html"
+  },
+  {
+    "revision": "5b6bd3d21ba8c5575c02",
+    "url": "/my-redux-todo-app/static/css/main.3ce6673a.chunk.css"
+  },
+  {
+    "revision": "7d7f482e93c0a657ad51",
+    "url": "/my-redux-todo-app/static/js/2.7ca6aaae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-redux-todo-app/static/js/2.7ca6aaae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6bd3d21ba8c5575c02",
+    "url": "/my-redux-todo-app/static/js/main.e947f508.chunk.js"
+  },
+  {
+    "revision": "9689c0e3e673adf50ab2",
+    "url": "/my-redux-todo-app/static/js/runtime-main.688b17bd.js"
+  }
+]);
